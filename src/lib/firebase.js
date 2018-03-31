9838644cc81910a90f4c645107333b01452b5ccd
Ronny Rook <info@ronnyrook.nl>
2018-03-31 14:28:51 +0200

    Add styled-components theme and firebase settings

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -2,31 +2,31 @@ import * as FirebaseModule from 'firebase';
 import firebaseConfig from '../constants/firebase';
 
 const {
-  apiKey,
-  authDomain,
-  databaseURL,
-  storageBucket,
-  messagingSenderId,
-} = firebaseConfig;
-
-let firebaseInitialized = false;
-
-if (
-  apiKey !== 'null' &&
-  authDomain !== 'null' &&
-  databaseURL !== 'null' &&
-  storageBucket !== 'null' &&
-  messagingSenderId !== 'null'
-) {
-  FirebaseModule.initializeApp({
     apiKey,
     authDomain,
     databaseURL,
     storageBucket,
     messagingSenderId,
-  });
+} = firebaseConfig;
 
-  firebaseInitialized = true;
+let firebaseInitialized = false;
+
+if (
+    apiKey !== 'null' &&
+  authDomain !== 'null' &&
+  databaseURL !== 'null' &&
+  storageBucket !== 'null' &&
+  messagingSenderId !== 'null'
+) {
+    FirebaseModule.initializeApp({
+        apiKey,
+        authDomain,
+        databaseURL,
+        storageBucket,
+        messagingSenderId,
+    });
+
+    firebaseInitialized = true;
 }
 
 export const FirebaseRef = firebaseInitialized ? FirebaseModule.database().ref() : null;
