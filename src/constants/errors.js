42c8de0f3e5d26061f41b46f542cf00dfff5001b
Ronny Rook <info@ronnyrook.nl>
2018-03-31 14:29:55 +0200

    Update constants, add styled-components theme

--- src/constants/errors.js
+++ src/constants/errors.js
@@ -1,21 +1,21 @@
 export default {
-  // Defaults
-  default: 'Hmm, an unknown error occured',
-  timeout: 'Server Timed Out. Check your internet connection',
-  invalidJson: 'Response returned is not valid JSON',
+    // Defaults
+    default: 'Hmm, an unknown error occured',
+    timeout: 'Server Timed Out. Check your internet connection',
+    invalidJson: 'Response returned is not valid JSON',
 
-  // Firebase Related
-  invalidFirebase: 'Firebase is not connected correctly',
+    // Firebase Related
+    invalidFirebase: 'Firebase is not connected correctly',
 
-  // Member
-  memberExists: 'Member already exists',
-  missingFirstName: 'First name is missing',
-  missingLastName: 'Last name is missing',
-  missingEmail: 'Email is missing',
-  missingPassword: 'Password is missing',
-  passwordsDontMatch: 'Passwords do not match',
+    // Member
+    memberExists: 'Member already exists',
+    missingFirstName: 'First name is missing',
+    missingLastName: 'Last name is missing',
+    missingEmail: 'Email is missing',
+    missingPassword: 'Password is missing',
+    passwordsDontMatch: 'Passwords do not match',
 
-  // Recipes
-  recipe404: 'Recipe not found',
-  missingMealId: 'Missing meal definition',
+    // Recipes
+    recipe404: 'Recipe not found',
+    missingMealId: 'Missing meal definition',
 };
---
+++ src/constants/theme.js
@@ -0,0 +1,5 @@
+export default {
+    blue: '#0b75e5',
+    green: '#90c727',
+    white: '#ffffff',
+};
