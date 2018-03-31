da54513cb357291de662433697da62bc2c441413
Ronny Rook <info@ronnyrook.nl>
2018-03-31 14:30:22 +0200

    Update App to include all providers

--- App.js
+++ App.js
@@ -1,9 +1,26 @@
 import React from 'react';
-import Root from './src/native/index';
-import configureStore from './src/store/index';
+import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/es/integration/react';
+import { ThemeProvider } from 'styled-components';
+
+import Loading from './src/native/components/Loading';
+import theme from './src/constants/theme';
+import configureStore from './src/store';
+import App from './src/native/index';
 
 const { persistor, store } = configureStore();
 
-export default function App() {
-  return <Root store={store} persistor={persistor} />;
-}
+const Root = () => (
+    <Provider store={store}>
+        <PersistGate
+            loading={<Loading />}
+            persistor={persistor}
+        >
+            <ThemeProvider theme={theme}>
+                <App />
+            </ThemeProvider>
+        </PersistGate>
+    </Provider>
+);
+
+export default Root;
