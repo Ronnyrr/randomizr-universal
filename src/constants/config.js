d3e2ee59400c99eb08e92a8467d82f3fffb162a2
Ronny Rook <info@ronnyrook.nl>
2018-04-10 15:10:13 +0200

    Update packages and index.js

--- src/constants/config.js
+++ src/constants/config.js
@@ -1,12 +1,12 @@
 const devMode = (process.env.NODE_ENV !== 'development');
 
 export default {
-  // App Details
-  appName: 'Randomizr',
+    // App Details
+    appName: 'Randomizr',
 
-  // Build Configuration - eg. Debug or Release?
-  DEV: devMode,
+    // Build Configuration - eg. Debug or Release?
+    DEV: devMode,
 
-  // Google Analytics - uses a 'dev' account while we're testing
-  gaTrackingId: (devMode) ? '' : '',
+    // Google Analytics - uses a 'dev' account while we're testing
+    gaTrackingId: (devMode) ? '' : '',
 };
