65fef8320cb65e5592a99c5fa2053ccbd728dd97
Ronny Rook <ronny.rook@labela.nl>
2018-04-11 10:29:53 +0200

    Merge config and constants

--- src/config/index.js
+++ src/config/index.js
@@ -1,5 +1,10 @@
 export const port = process.env.PORT || 3000;
 export const env = process.env.NODE_ENV || 'development';
 
+export const appSettings = {
+    appName: 'Randomizr',
+    gaTrackingId: (env === 'development') ? '' : '', // Google Analytics - uses a 'dev' account while we're testing
+};
+
 // Toggle Serverside rendering
 export const SSR = false;
