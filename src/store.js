d6a3b404ca00e98f5c2e5d4759bb525a3a8d356f
Ronny Rook <ronny.rook@labela.nl>
2018-04-12 09:40:05 +0200

    Fix __CLIENT__ error in store on mobile

--- src/store.js
+++ src/store.js
@@ -3,13 +3,19 @@ import { combineReducers, createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 
 import * as appReducers from './ducks';
+import globals from './config/globals';
+
+const globs = globals('client');
+console.log(globs);
+// console.log(__DEV__);
+// console.log(__CLIENT__);
 
 let middleware = applyMiddleware(thunk);
 
 // default store
 const reducers = combineReducers({ ...appReducers });
 
-if (__DEV__ && __CLIENT__ && typeof window.devToolsExtension === 'function') {
+if (__DEV__ && (globs.__CLIENT__ || __CLIENT__) && typeof window.devToolsExtension === 'function') {
     middleware = compose(middleware, window.devToolsExtension());
 }
 
