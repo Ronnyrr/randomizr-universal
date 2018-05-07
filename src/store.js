80f85fa1ddfb3adb5ba364919f4da568423e6ff6
Ronny Rook <ronny.rook@labela.nl>
2018-05-07 10:28:59 +0200

    Fix window undefined bug on producion

--- src/store.js
+++ src/store.js
@@ -6,17 +6,13 @@ import * as appReducers from './ducks';
 import globals from './config/globals';
 
 const globs = globals('client');
-// console.log(globs);
-// console.log(__DEV__);
-// console.log(__CLIENT__);
-
 let middleware = applyMiddleware(thunk);
 
 // default store
 const reducers = combineReducers({ ...appReducers });
 
-if (__DEV__ && (globs.__CLIENT__ || __CLIENT__) && typeof window.devToolsExtension === 'function') {
-    middleware = compose(middleware, window.devToolsExtension());
+if (__DEV__ && (globs.__CLIENT__ || __CLIENT__) && typeof window !== 'undefined' && window.devToolsExtension === 'function') {
+  middleware = compose(middleware, window.devToolsExtension());
 }
 
 export default createStore(reducers, middleware);
